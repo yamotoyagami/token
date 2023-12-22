0c408846fa6836b71a223fb73cc271277e7cebca
yamotoyagami <149977527+yamotoyagami@users.noreply.github.com>
2023-12-22 15:09:38 +0100

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Token is ERC20, ERC20Permit {
+    constructor() ERC20("token", "TKN") ERC20Permit("token") {}
+}
